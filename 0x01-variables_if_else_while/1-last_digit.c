e6b66efb8e7e734943a3b70ff91e770120d8ea3b
MuneneRubia <ianmunene78790@gmail.com>
2023-03-17 15:31:48 -0700

    lo level

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /**
- * main - print the alphabet in lowercase followed by a new line
+ * main - prints the last digit of the random numebr generated
+ * Stores the number in variable n
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
+	int n;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
 	{
-		putchar(ch);
+		printf("and is greater than 5\n");
 	}
-	putchar('\n');
+	else if (n % 10 == 0)
+	{
+		printf("and is 0\n");
+	}
+	else if (n % 10 < 6 && n % 10 != 0)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
 	return (0);
 }

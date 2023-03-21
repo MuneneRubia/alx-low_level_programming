4c74a5dacd9a6415d4247d251dd65f5b15b91e0d
MuneneRubia <ianmunene78790@gmail.com>
2023-03-21 10:07:27 -0700

    Nested

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,38 @@
+/* _putchar.c */
+int _putchar(char);
+
+/*print alphabet*/
+void print_alphabet(void);
+
+/*print alphabet 10 times*/
+void print_alphabet_x10(void);
+
+/* function checks for lowercase character*/
+int _islower(int c);
+
+/* function checks fir lowercase characters*/
+int _isalpha(int c);
+
+/* functions that prints the sign of a number*/
+int print_sign(int n);
+
+/* functions that computes the absolute value of an integer*/
+int _abs(int);
+
+/* function that prints the last digit of a number*/
+int print_last_digit(int);
+
+/* functionthat prints every minute of the day*/
+void jack_bauer(void);
+
+/* function that prints the multiplication table of 9*/
+void times_table(void);
+
+/* function that add two integers*/
+int add(int, int);
+
+/* function that prints all natural numbers*/
+void print_to_98(int n);
+
+/* function that prints the n times table*/
+void print_times_table(int n);

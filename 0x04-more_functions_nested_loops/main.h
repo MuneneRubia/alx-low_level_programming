b6c7992ab292c56e07a85db9d82cea78a0f5d048
MuneneRubia <ianmunene78790@gmail.com>
2023-03-23 05:14:51 -0700

    more function nested loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,23 +10,26 @@ int _isdigit(int  c);
 /* functions that multiply two integers*/
 int mul(int a, int b);
 
-/*function that prints the numbers 0 to 9*/
+/*function that prints numbers 0 to 9*/
 void print_numbers(void);
 
-/*function that prints the numbers*/
+/*function that prints most numbers*/
+void print_most_numbers(void);
+
+/*function that prints 10 times the number*/
 void more_numbers(void);
 
-/*function that draws a straight line*/
+/*function that draw a straight line*/
 void print_line(int n);
 
-/*function that draw a diagonal line*/
+/*function that draws a diagonal line*/
 void print_diagonal(int n);
 
-/*print the squares*/
+/*function prints a square*/
 void print_square(int size);
 
 /*function prints a triangle*/
 void print_triangle(int size);
 
-/*function prints an integer*/
+/*function that prints an integer*/
 void print_number(int n);

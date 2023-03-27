0ca056df05e146e97609ba2e4fda7668601373ec
MuneneRubia <ianmunene78790@gmail.com>
2023-03-27 11:23:54 -0700

    pointer arrays strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,3 +3,6 @@ int _putchar (char c);
 
 /* function that points to an int as parameter*/
 void reset_to_98(int *n);
+
+/*function that swaps the value of two integers*/
+void swap_int(int *a, int *b);

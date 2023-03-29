4486fcd3673d2b815a88972241231e0a7dfff0e1
MuneneRubia <ianmunene78790@gmail.com>
2023-03-29 11:33:53 -0700

    pointer array strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,4 +7,7 @@ char *_strcat(char *dest, char *src);
 /* function concatenates two strings*/
 char *_strncat(char *dest, char *src, int n);
 
+/*function that copies a string*/
+char *_strncpy(char *dest, char *src, int n);
+
 

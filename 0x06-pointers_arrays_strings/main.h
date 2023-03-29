560bab6ad273ecec95afa5dbb23ec54cb8588659
MuneneRubia <ianmunene78790@gmail.com>
2023-03-29 11:23:09 -0700

    pointer array strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+/*prints _putchar*/
+int _putchar (char c);
+
+/* function that concatenates 2 strings*/
+char *_strcat(char *dest, char *src);
+
+/* function concatenates two strings*/
+char *_strncat(char *dest, char *src, int n);
+
+

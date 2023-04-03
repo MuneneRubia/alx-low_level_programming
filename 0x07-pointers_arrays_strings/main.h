43df8dee3db29545469d40eb8356886ecc16c962
MuneneRubia <ianmunene78790@gmail.com>
2023-04-03 07:18:43 -0700

    More pointer arrays strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -16,3 +16,6 @@ unsigned int _strspn(char *s, char *accept);
 /*function that searches a string for any of a set of bytes*/
 char *_strpbrk(char *s, char *accept);
 
+/*function that locates a substring*/
+char *_strstr(char *haystack, char *needle);
+

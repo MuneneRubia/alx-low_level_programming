313b8bb51ac5157fc841627ab47e0c508991f227
MuneneRubia <ianmunene78790@gmail.com>
2023-04-03 07:29:38 -0700

    More pointer arrays strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -25,3 +25,6 @@ void print_chessboard(char (*a)[8]);
 /*function that prints the sum of the two diagonals of a square matrix of integers*/
 void print_diagsums(int *a, int size);
 
+/*function that sets the value of a pointer to a char*/
+void set_string(char **s, char *to);
+

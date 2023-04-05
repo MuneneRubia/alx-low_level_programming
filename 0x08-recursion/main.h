1a43cb6c0a6f754ece8e73f0741ed49b7dcf3bd8
MuneneRubia <ianmunene78790@gmail.com>
2023-04-05 11:05:13 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,3 +6,6 @@ void _puts_recursion(char *s);
 
 /*function that prints a string in reverse*/
 void _print_rev_recursion(char *s);
+
+/*function that returns the length of a string*/
+int _strlen_recursion(char *s);

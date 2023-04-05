cb7069c2f1272acfeb2106ec5549df960fa6cc44
MuneneRubia <ianmunene78790@gmail.com>
2023-04-05 11:15:36 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,6 @@ void _print_rev_recursion(char *s);
 
 /*function that returns the length of a string*/
 int _strlen_recursion(char *s);
+
+/*function that returns the factorial of a given number*/
+int factorial(int n);

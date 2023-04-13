ebbe2d03eca9ef1740fe7347f017514585694569
MuneneRubia <ianmunene78790@gmail.com>
2023-04-13 10:58:15 -0700

    more malloc free

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+/* prints putchar*/
+int _putchar (char c);
+
+/*Function that allocates memory*/
+void *malloc_checked(unsigned int b);
+
+/*function concatenatestwo strings*/
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* function that allocates memory for an array*/
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* function that creates an array of integers*/
+int *array_range(int min, int max);
+
+/* function that reallocates a memory block*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
